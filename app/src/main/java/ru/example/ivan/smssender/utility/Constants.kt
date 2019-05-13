dd430255494c1ed1dbbd950eb539144f10a7535f
Ivan <ivan.r.makarov@yandex.ru>
2019-05-13 16:36:36 +0300

    ADD -- Constants class

---
+++ app/src/main/java/ru/example/ivan/smssender/utility/Constants.kt
@@ -0,0 +1,11 @@
+package ru.example.ivan.smssender.utility
+
+class Constants {
+    companion object {
+        //bundle keys
+        const val KEY_TEMPLATE = "template"
+
+        //broadcast keys
+        const val SELECTED_TEMPLATE = "selected-template"
+    }
+}
